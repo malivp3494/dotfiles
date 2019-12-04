9bbc8dede5ac5c93b8b59678bee36e36536a2775
Vaibhav Mali <malivp3494@gmail.com>
2019-12-04 10:00:57 +0530

    added script to backup configs

---
+++ config_files/backup_configs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "backup configs"
+echo "ZSHRC..."
+cat ~/.zshrc > ZSHRC
+echo "VIMRC..."
+cat ~/.vimrc > VIMRC
+echo "TMUX.CONF..."
+cat ~/.tmux.conf > TMUX.CONF
+echo "REDSHIFT.CONF"
+cat ~/.config/redshift.conf > REDSHIFT.CONF
+echo "etc_rc.local..."
+cat /etc/rc.local > etc_rc.local
+echo "etc_environment..."
+cat /etc/environment > etc_environment
+echo "apt_packages..."
+comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u) > apt_packages.txt
+echo "vscode_extensions..."
+code --list-extensions > vscode_extensions.txt
+echo "Backup successful!"
+
